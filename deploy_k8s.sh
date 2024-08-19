00e6028d5f726beb1cd5fd2bfeb948c627a4236d
Student <markuj5@gmail.com>
2024-08-19 14:36:01 +0200

    Adding day1 files

---
+++ deploy_k8s.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "[TASK 1] Create module configuration file for containerd"
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+# load modules
+modprobe overlay >/dev/null 2>&1
+modprobe br_netfilter >/dev/null 2>&1
+
+echo "[TASK 2] Set system configurations for Kubernetes networking"
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# apply new settings
+sysctl --system >/dev/null 2>&1
+
+echo "[TASK 3] Install containerd runtime"
+apt-get update >/dev/null 2>&1
+apt-get install -y containerd >/dev/null 2>&1
+
+echo "[TASK 4] Generate default containerd configuration and save to the newly created default file"
+mkdir -p /etc/containerd >/dev/null 2>&1
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+
+echo "[TASK 5] Update containerd option for System Cgroup"
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml >/dev/null 2>&1
+
+# apply new settings
+systemctl restart containerd >/dev/null 2>&1
+
+echo "[TASK 6] Disable SWAP"
+swapoff -a >/dev/null 2>&1
+sed -i 's/.* none.* swap.* sw.*/#&/g' /etc/fstab >/dev/null 2>&1
+
+echo "[TASK 7] Install depencdency packages"
+apt-get update >/dev/null 2>&1
+apt-get install -y apt-transport-https curl >/dev/null 2>&1
+
+echo "[TASK 8] Add GPG key for K8s repo"
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+ >/dev/null 2>&1
+
+echo "[TASK 9] Add K8s repository"
+mkdir -p /etc/apt/keyrings
+if [[ $# -gt 3 ]]; then
+        curl -x $4 -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
+else
+        curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
+fi
+
